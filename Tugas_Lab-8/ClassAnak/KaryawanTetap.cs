ffdd2b6dd9c02463ae66bd9b001f194ae5ab2aab
aniskniaa <rnia578@gmail.com>
2020-06-23 10:14:48 +0700

    inisialisasi project

---
+++ Tugas_Lab-8/ClassAnak/KaryawanTetap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tugas_Lab_8.ClassInduk;
+
+namespace Tugas_Lab_8.ClassAnak
+{
+    class KaryawanTetap : Karyawan
+    {
+        public double GajiBulanan { get; set; }
+
+        public override double Gaji()
+        {
+           return GajiBulanan;
+        }
+    }
+}
---
+++ Tugas_Lab-8/ClassInduk/Karyawan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas_Lab_8.ClassInduk
+{
+    public abstract class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+
+        public abstract double Gaji();
+
+    }
+}
